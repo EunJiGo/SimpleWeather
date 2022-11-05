b125111ea3c8322188005abcdb23c862311a0f5b
GoEunJi <jieunko1118@gmail.com>
2022-11-06 00:18:00 +0900

    Initial Commit

---
+++ test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by 고은지 on 2022/11/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
